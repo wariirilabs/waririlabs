9cdff474134323aefb2294b23e6111cce3ca4d0b
RobertMiski <72018451+RobertMiski@users.noreply.github.com>
2021-02-22 16:10:33 +0000

    da

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
 
 
 document.addEventListener('DOMContentLoaded', function() {
---
+++ login.js
@@ -0,0 +1,31 @@
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+  
+  
+
+  let stock = document.querySelector(".stock");
+  let order = document.querySelector(".order");
+
+stock.addEventListener('click', function (){
+  let forma = document.querySelector(".forma");
+  
+  forma.classList.remove("hide");
+  forma.classList.add("show");
+  this.classList.add('active');
+  order.classList.remove("active");
+
+})
+
+order.addEventListener('click', function (){
+  let forma = document.querySelector(".forma");
+  
+  forma.classList.remove("show");
+  forma.classList.add("hide");
+  this.classList.add('active');
+  stock.classList.remove("active");
+
+})
